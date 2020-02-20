dc0363ccba59d11c82562cf9eb1bc096fd8d125a
sanya <79fz270704@gmail.com>
2020-02-20 09:03:23 +0300

    fix

--- contacts/forms.py
+++ contacts/forms.py
@@ -3,14 +3,12 @@ from .tasks import send_email_task
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(min_length=3, max_length=100)
     email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(widget=forms.Textarea, max_length=1000)
 
     def send_email(self):
-        # send email using the self.cleaned_data dictionary
         name = self.cleaned_data.get('name')
         email = self.cleaned_data.get('email')
         message = self.cleaned_data.get('message')
         send_email_task.delay(name, email, message)
-        print('sending messages')
